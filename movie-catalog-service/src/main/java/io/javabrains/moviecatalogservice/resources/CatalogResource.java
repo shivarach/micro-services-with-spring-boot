ac3c4e916bdfc9bf193894ce1dd88c5cbbcd729a
Siva Rachakonda <shiva.rachakonda139@gmail.com>
2019-10-27 23:37:28 +0530

    Add Hystrix circuit breaker

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/CatalogResource.java
@@ -21,22 +21,21 @@ import java.util.stream.Collectors;
 public class CatalogResource {
 
     @Autowired
-    private RestTemplate restTemplate;
+    private RatingInfo ratingInfo;
 
     @Autowired
-    WebClient.Builder webClientBuilder;
+    private MovieInfo movieInfo;
+
+   /* @Autowired
+    WebClient.Builder webClientBuilder;*/
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
+        UserRating userRating = ratingInfo.getUserRating(userId);
 
         return userRating.getRatings().stream()
-                .map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-                    return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
-                })
-                .collect(Collectors.toList());
+                .map(rating -> movieInfo.getCatalogItem(rating)).collect(Collectors.toList());
 
     }
 }
---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/RatingInfo.java
@@ -0,0 +1,37 @@
+package io.javabrains.moviecatalogservice.resources;
+
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import io.javabrains.moviecatalogservice.models.Rating;
+import io.javabrains.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@Service
+public class RatingInfo {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public RatingInfo(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+    public UserRating getUserRating(String userId) {
+        return restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
+    }
+
+    private UserRating getFallbackUserRating(String userId) {
+        final UserRating userRating = new UserRating();
+        userRating.setUserId(userId);
+        userRating.setRatings(Arrays.asList(
+                new Rating("0", 0)
+        ));
+        return userRating;
+    }
+
+}
